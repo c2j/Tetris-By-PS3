9166961da1a08a36bcc5e61b3eed65ee379e05ec
c2j <c2j@ps3.local>
2026-09-18 15:49:17 +0000

    Revert to PowerPC termios constants (ISIG=0x80, ICANON=0x100, VMIN=5, VTIME=7)

---
+++ target/debug/deps/tetris-7d05ad8e4c183ec8.d
@@ -0,0 +1,5 @@
+/home/c2j/workspace/rust-tetris/target/debug/deps/tetris-7d05ad8e4c183ec8.d: src/main.rs
+
+/home/c2j/workspace/rust-tetris/target/debug/deps/tetris-7d05ad8e4c183ec8: src/main.rs
+
+src/main.rs:
---
+++ target/debug/tetris.d
@@ -0,0 +1,1 @@
+/home/c2j/workspace/rust-tetris/target/debug/tetris: /home/c2j/workspace/rust-tetris/src/main.rs
